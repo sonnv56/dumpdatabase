4f1f0d1e15ddeb5b657caa4f3dd6df83751f6c05
Son <sonnguyen@sonnguyen.local>
2017-04-06 15:05:39 +0700

    Modifed Main.java : disable html escaping

--- src/main/java/services/Main.java
+++ src/main/java/services/Main.java
@@ -1,7 +1,8 @@
 package services;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import pojos.Author;
 import pojos.Publication;
@@ -25,7 +27,7 @@ public class Main {
 	List<Publication> publications = new ArrayList<Publication>();
 
 	public static void main(String args[]) throws IOException {
-//		Document doc = Jsoup.connect(LINK).get();
+		// Document doc = Jsoup.connect(LINK).get();
 		Document doc = Jsoup.parse(new File(FILE_INPUT), "UTF-8");
 		List<Publication> publications = new ArrayList<Publication>();
 		Elements articles = doc.getElementsByTag("tr");
@@ -35,25 +37,31 @@ public class Main {
 				publications.add(p);
 			}
 		}
-		String jsons = new Gson().toJson(publications);
+		Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+		String jsons = gson.toJson(publications);
 		String filePath = FILE_DESTINATION;
 		writeJSONTextToFile(jsons, filePath);
 	}
 
 	private static void writeJSONTextToFile(String content, String filePath) {
-		File file = new File(filePath);
-		try (FileOutputStream fop = new FileOutputStream(file)) {
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-			byte[] contentInBytes = content.getBytes("UTF-8");
-			fop.write(contentInBytes);
-			fop.flush();
-			fop.close();
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+			bw.write(content);
 			System.out.println("Done");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static String convertHexToString(String hex) {
+		StringBuilder sb = new StringBuilder();
+		StringBuilder temp = new StringBuilder();
+		for (int i = 0; i < hex.length() - 1; i += 2) {
+			String output = hex.substring(i, (i + 2));
+			int decimal = Integer.parseInt(output, 16);
+			sb.append((char) decimal);
+			temp.append(decimal);
+		}
+		return sb.toString();
 	}
 
 	private static Publication getPublicationFromAttrDOM(Element articleDOM) {
@@ -72,9 +80,10 @@ public class Main {
 			String abstracts = attrs.get(6).text();
 			String journalName = attrs.get(1).text();
 			int year = -1;
-			try{
+			try {
 				year = Integer.parseInt(attrs.get(4).text());
-			}catch(NumberFormatException e){}
+			} catch (NumberFormatException e) {
+			}
 			p = new Publication(title, abstracts, uri, authors, journalName, volume, number, year, source, usable, DOI,
 					citedList);
 		}
